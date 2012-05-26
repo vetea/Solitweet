b63b6b47305bb8442c2613ccdb766836dc1c27ed
Sebastien Bourdu <github@ekkans.com>
2012-05-26 14:59:47 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Solitweet::Application.config.secret_token = '1a4d674a52a7649f357210b599b683221fb0a6ce48ab90578d581f95b9a828def8a6472c7b3dd344420e3e034f370a72ea1702a0c8a963f84322a80dc5f97437'
