0c6bd37f571f728606c489f56afd19fad7435971
Sebastien Bourdu <github@ekkans.com>
2012-07-02 14:42:33 +0200

    Updated wording

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,12 @@
+# encoding: utf-8
+
 module PagesHelper
+
+  def title
+    html = "Avec SenseTwitt, soutiens les propositions du Mouvement des entrepreneurs sociaux"
+    html << ' (<a href="http://www.mouves.org/" target="_blank">Mouves</a>) '
+    html << "pour développer l'entrepreunariat social"
+    html.html_safe
+  end
+
 end
